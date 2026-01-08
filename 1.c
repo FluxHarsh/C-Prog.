4b3ff1f1ba4160878472165f0ea60c59c1bb9b56
Harsh Jagtap <harshjagtap843@gmail.com>
2026-01-08 22:24:34 +0530

    c code

---
+++ 1.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+// learing how to use scanf and input thing
+// int main(){
+
+//     int a,b ;
+//     printf("enter a");
+//     scanf("%d", &a);
+
+//     printf("enter b");
+//     scanf("%d", &b);
+    
+//     int sum = a+b;
+//     printf("sum is: %d", sum);
+
+//     return 0;
+// }
+
+
+//Q1)
+
+// int main() {
+//     int side;
+//     printf("enter side length of the square");
+//     scanf("%d", &side);
+
+
+//     printf("The Area of the square is: %d", side*side);
+
+//     return 0;
+// }
+
+//Q2)
+int main() {
+    float radius;
+    printf("enter radius");
+    scanf("%f", &radius);
+
+
+    printf("The Area of the circle is: %f", 3.14*radius*radius);
+
+    return 0;
+}
+
